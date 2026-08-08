de20137300775cfb0bfe5a1144d194ee0698295c
Hadiah-Batool <hdbatool779@gmail.com>
2026-08-08 16:48:06 +0500

    Shit

--- rehnumai/macos/Flutter/GeneratedPluginRegistrant.swift
+++ rehnumai/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import isar_flutter_libs
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
